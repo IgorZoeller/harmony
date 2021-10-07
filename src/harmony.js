a4bbcf0e01c40989392fb24ad250a094eb5e4bd5
Igor <igorzoeller@usp.br>
2021-10-07 00:13:54 -0300

    Add Command and Client classes

--- src/harmony.js
+++ src/harmony.js
@@ -1,10 +1,10 @@
 console.clear();
+
 require('dotenv').config()
-const Discord = require('discord.js');
+const Client  = require('./structures/Client.js');
+const Command = require('./structures/Command.js');
 
-const intents = new Discord.Intents(32767);
-
-const client = new Discord.Client({intents});
+const client = new Client();
 
 client.on('ready', () => console.log('Harmonic convergence is upon us!'));
 
