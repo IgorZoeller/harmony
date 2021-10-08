f4d01ca78fc53931737b8bf3858ea4bef4a3b0d7
Igor <igorzoeller@usp.br>
2021-10-08 00:54:33 -0300

    Improve path handling

--- src/scripts/new_command.py
+++ src/scripts/new_command.py
@@ -2,11 +2,17 @@
 import sys
 import os
 
+# Take the function parameter as the new command name
 command_name = sys.argv[1]
 
 file_name = command_name + ".js"
 
+# Go to the Project scripts folder, this makes sure that the
+# command file will be created at the correct directory
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 command_folder = "../commands"
+
+path = os.path.join(command_folder, file_name)
 
 command_template = ["const Command = require(\"../structures/Command.js\");\n\n",
                     "module.exports = new Command({\n",
@@ -16,5 +22,9 @@ command_template = ["const Command = require(\"../structures/Command.js\");\n\n"
                     "    status: \n",
                     "})\n"]
 
-with open(os.path.join(command_folder, file_name), "a") as f:
-    f.writelines(command_template)
+# Check if the command already exists to prevent overwriting
+if not os.path.exists(path):
+    with open(path, "w") as f:
+        f.writelines(command_template)
+else:
+    print("Command \"{}\" already exists.".format(command_name))
