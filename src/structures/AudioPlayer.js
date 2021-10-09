248387393eea2f4816ba9082fe15581802315c64
Igor <igorzoeller@usp.br>
2021-10-09 20:38:21 -0300

    Push small changes on audio player [temp]

--- src/structures/AudioPlayer.js
+++ src/structures/AudioPlayer.js
@@ -5,11 +5,23 @@ class AudioPlayer extends Voice.AudioPlayer {
     constructor(options){
         super(options);
         // The Audio Player has a queue for musics
-        const queue = new AudioQueue();
+        this.queue = new AudioQueue();
 
         this.connection;
+        this.subscription;
     }
 
-    connectVoice(VoiceChannel,)
+    // For now, the audio player will only be responsible of
+    // joining the channel. The subscription will be either the
+    // client or the command responsibility.
+    callVoice(voiceChannel){
+
+        this.connection = voice.joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+        });
+
+    }
 
 }
